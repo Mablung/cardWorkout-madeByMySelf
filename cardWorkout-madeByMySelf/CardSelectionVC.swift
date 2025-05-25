08e255ab4c9e6a8135efc1dcc1ff992e9850b168
mablung <berkedemirel61@gmail.com>
2025-05-26 02:23:06 +0300

    Initial Commit

---
+++ cardWorkout-madeByMySelf/CardSelectionVC.swift
@@ -0,0 +1,20 @@
+//
+//  CardSelectionVC.swift
+//  cardWorkout-madeByMySelf
+//
+//  Created by Nurettin Berke Demirel on 26.05.2025.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+    
+
+
+}
