fc572f22125ec7726246799658cfc62c58f73b4d
mablung <berkedemirel61@gmail.com>
2025-05-26 03:28:02 +0300

    Added card imageview to main page.

---
+++ cardWorkout-madeByMySelf/CWButton.swift
@@ -0,0 +1,20 @@
+//
+//  CWButton.swift
+//  cardWorkout-madeByMySelf
+//
+//  Created by Nurettin Berke Demirel on 26.05.2025.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ cardWorkout-madeByMySelf/CardDeck.swift
@@ -0,0 +1,8 @@
+//
+//  CardDeck.swift
+//  cardWorkout-madeByMySelf
+//
+//  Created by Nurettin Berke Demirel on 26.05.2025.
+//
+
+import Foundation
