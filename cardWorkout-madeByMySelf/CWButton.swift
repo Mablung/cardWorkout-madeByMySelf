f4e971c2917678f80013c045cd2410793da755f4
mablung <berkedemirel61@gmail.com>
2025-05-26 03:42:16 +0300

    added stop button on main page

--- cardWorkout-madeByMySelf/CWButton.swift
+++ cardWorkout-madeByMySelf/CWButton.swift
@@ -8,13 +8,43 @@
 import UIKit
 
 class CWButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init (frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+        
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(color: UIColor,title: String,cornerStyle: Configuration.CornerStyle){
+        super.init(frame: .zero)
+        
+        configuration = .filled()
+        configuration?.baseForegroundColor = color
+        configuration?.baseBackgroundColor = color
+        configuration?.cornerStyle = cornerStyle
+        
+        setTitle(title, for: .normal)
+        
+        configure()
 
+    }
+    
+    
+    func configure() {
+        self.layer.cornerRadius = 8
+        self.layer.masksToBounds = true
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        self.setTitleColor(.white, for: .normal)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        }
 }
+
+
+
+
+
--- cardWorkout-madeByMySelf/CardSelectionVC.swift
+++ cardWorkout-madeByMySelf/CardSelectionVC.swift
@@ -8,13 +8,65 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
+    
+    let cardArea = UIImageView()
+    
+    let stopButton = CWButton(color: .red, title: "Stop", cornerStyle: .capsule)
+    let rulesButton = CWButton(color: .blue, title: "Rules", cornerStyle: .medium)
+    let restartButton = CWButton(color: .green, title: "Restart", cornerStyle: .medium)
+    
+    let cardDeck: [UIImage] = Deck.allValues
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
+        
+        configureUI()
+        
     }
     
+    
+    func configureUI (){
+        
+        
+        view.backgroundColor = .systemBackground
+        configureCardArea()
+        configureStopButton()
+        
+    }
+    
+    func configureCardArea() {
+        view.addSubview(cardArea)
+        
+        cardArea.translatesAutoresizingMaskIntoConstraints = false
+        cardArea.image =  UIImage(named:"AS")
+        
+        NSLayoutConstraint.activate([
+            cardArea.widthAnchor.constraint(equalToConstant: 250),
+            cardArea.heightAnchor.constraint(equalToConstant: 350),
+            cardArea.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardArea.centerYAnchor.constraint(equalTo: view.centerYAnchor,constant: -75)
 
-
+        ])
+    }
+    
+    func configureStopButton(){
+        view.addSubview(stopButton)
+        
+        
+        NSLayoutConstraint.activate([
+            stopButton.widthAnchor.constraint(equalToConstant: 260),
+            stopButton.heightAnchor.constraint(equalToConstant: 50),
+            stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stopButton.topAnchor.constraint(equalTo: cardArea.bottomAnchor, constant: 30)
+            
+            
+            
+           
+            
+            
+        ])
+    }
+    
+    
 }
