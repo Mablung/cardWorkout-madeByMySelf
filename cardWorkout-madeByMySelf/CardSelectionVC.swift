9cbe8657c85de7f452ec0ebacc30db01a32d4334
mablung <berkedemirel61@gmail.com>
2025-05-26 04:02:30 +0300

    added all buttons on main page. added stop and restart button targets.

--- cardWorkout-madeByMySelf/CardSelectionVC.swift
+++ cardWorkout-madeByMySelf/CardSelectionVC.swift
@@ -17,11 +17,35 @@ class CardSelectionVC: UIViewController {
     
     let cardDeck: [UIImage] = Deck.allValues
     
+    var time: Timer?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        activateTimer()
+        
+    }
+    
+    func activateTimer(){
+        
+        time = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(changeCard), userInfo: nil, repeats: true)
+    }
+    
+    @objc func changeCard(){
+        cardArea.image = cardDeck.randomElement()
+        
+    }
+    
+    @objc func stopTimer(){
+        time?.invalidate( )
+    }
+    
+    @objc func restartTimer(){
+        
+        time?.invalidate( )
+        activateTimer( )
         
     }
     
@@ -32,6 +56,8 @@ class CardSelectionVC: UIViewController {
         view.backgroundColor = .systemBackground
         configureCardArea()
         configureStopButton()
+        configureRestartButton()
+        configureRulesButton()
         
     }
     
@@ -52,6 +78,8 @@ class CardSelectionVC: UIViewController {
     
     func configureStopButton(){
         view.addSubview(stopButton)
+        stopButton.addTarget(self, action: #selector(stopTimer), for: .touchUpInside)
+        
         
         
         NSLayoutConstraint.activate([
@@ -59,13 +87,40 @@ class CardSelectionVC: UIViewController {
             stopButton.heightAnchor.constraint(equalToConstant: 50),
             stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stopButton.topAnchor.constraint(equalTo: cardArea.bottomAnchor, constant: 30)
-            
-            
-            
-           
+        ])
+    }
+    
+    
+    func configureRestartButton(){
+        view.addSubview(restartButton)
+        restartButton.addTarget(self, action: #selector(restartTimer), for: .touchUpInside)
+        
+        
+        NSLayoutConstraint.activate([
+            restartButton.widthAnchor.constraint(equalToConstant: 110),
+            restartButton.heightAnchor.constraint(equalToConstant: 50),
+            restartButton.leadingAnchor.constraint(equalTo: stopButton.leadingAnchor),
+            restartButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 10)
+        ])
+    }
+    
+    
+    func configureRulesButton(){
+        
+        view.addSubview(rulesButton)
+        
+        
+        NSLayoutConstraint.activate([
+            rulesButton.widthAnchor.constraint(equalToConstant: 110),
+            rulesButton.heightAnchor.constraint(equalToConstant: 50),
+            rulesButton.trailingAnchor.constraint(equalTo: stopButton.trailingAnchor),
+            rulesButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 10),
             
             
         ])
+        
+        
+        
     }
     
     
